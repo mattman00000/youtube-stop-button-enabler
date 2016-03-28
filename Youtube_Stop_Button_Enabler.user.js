9c31219427a8522f1eb252c232f234e18ca5f179
mattman00000 <mattman00000@gmail.com>
2016-03-28 16:13:28 -0700

    0.0.5

--- Youtube_Stop_Button_Enabler.user.js
+++ Youtube_Stop_Button_Enabler.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Stop Button Enabler
 // @namespace    https://github.com/mattman00000
-// @version      0.0.4
+// @version      0.0.5
 // @description  stop playback with keyboard stop button or F7
 // @author       mattman00000
 // @match        *://www.youtube.com/*
@@ -14,5 +14,5 @@
     // Your code here...
     // F7 = 118
     // [Stop] = 178
-	window.addEventListener("keydown",function(a){if (a.keyCode == 178 || a.keyCode == 118) document.getElementsByTagName("video")[0].pause();});
+	window.addEventListener("keydown",function(a){if (a.key == "MediaStop" || a.key == "F7") document.getElementsByTagName("video")[0].pause();});
 })();
